af6d919df246895cd106e0e6952a5fec3530a608
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-03-21 23:51:55 +0100

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./css/main.css"
+import Layout from "../components/layout"
+
+const ErrorPage = () => {
+  return (
+    <Layout>
+      <div>
+        This is a simple 404 page - if you come here that means something went
+        wrong.
+      </div>
+    </Layout>
+  )
+}
+
+export default ErrorPage
