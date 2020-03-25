ed7785b66bfc74900b14058f3d50ff2184e038cd
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-03-25 21:38:44 +0100

    Add fallback video frames

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import aboutAnimation from "../assets/videos/about_small.mp4"
+import aboutFrame from "../assets/videos/about.jpg"
 import instaIcon from "../assets/images/instagram.svg"
 import youtubeIcon from "../assets/images/youtube.svg"
 
@@ -8,7 +9,10 @@ const About = () => {
   return (
     <Layout color="#4f2223" site="about">
       <div className="container">
-        <video autoPlay="autoplay" muted loop src={aboutAnimation}></video>
+        <video autoPlay="autoplay" muted loop>
+          <source src={aboutAnimation} />
+          <img src={aboutFrame} alt="A still frame while video loads" />
+        </video>
         <div className="social-media-bar">
           <a href="https://www.instagram.com">
             <img src={instaIcon} alt="instagram-icon" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import mainAnimation from "../assets/videos/main_small.mp4"
+import mainFrame from "../assets/videos/main.jpg"
 import Layout from "../components/layout"
 
 const Home = () => {
   return (
     <Layout color="white" site="home">
       <div className="container">
-        <video autoPlay="autoplay" muted loop src={mainAnimation}></video>
+        <video autoPlay="autoplay" muted loop>
+          <source src={mainAnimation} />
+          <img src={mainFrame} alt="A still frame while video loads" />
+        </video>
       </div>
     </Layout>
   )
