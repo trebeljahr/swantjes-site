9c38503c43f5feadaaa2789e33c30a9392ed0da0
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-03-25 21:10:30 +0100

    Remove fontawesome

--- src/components/header.js
+++ src/components/header.js
@@ -8,10 +8,6 @@ const Head = () => {
         href="https://fonts.googleapis.com/css?family=Josefin+Sans:100&display=swap"
         rel="stylesheet"
       ></link>
-      <script
-        src="https://kit.fontawesome.com/ef7381bf93.js"
-        crossorigin="anonymous"
-      ></script>
     </Helmet>
   )
 }
