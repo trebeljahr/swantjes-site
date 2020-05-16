be700d5ef2b778c5cabbd326a7247d6d2cf8501c
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-05-16 23:05:14 +0200

    Make parsing of films page a lot more robust

--- src/templates/film-template.js
+++ src/templates/film-template.js
@@ -1,126 +1,48 @@
-import React, { createRef, Component } from "react"
+import React, { useEffect, useRef } from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
-import { Loader } from "../components/Loader"
 
-class FilmTemplate extends Component {
-  constructor(props) {
-    super(props)
-    const post = this.props.data.ghostPost
-    const filmRegex = new RegExp("<iframe.*?</iframe>", "g")
-    const textRegex = new RegExp("<p>.*?</p>", "g")
-    const films = post.html
-      .match(filmRegex)
-      .map(
-        (html, index) =>
-          html.substring(0, 7) + ` id=film-${index}` + html.substring(7)
-      )
-    const texts = post.html.match(textRegex)
-    this.state = {
-      post,
-      things: films.map((film, i) => ({
-        film,
-        text: texts[i],
-        id: "" + i,
-      })),
-      activeThing: { id: null, ratio: 0 },
-      loading: films.reduce((acc, _, i) => {
-        return { ...acc, [`film-${i}`]: true }
-      }, {}),
-    }
+const FilmTemplate = ({ data }) => {
+  const rootRef = useRef(null)
+  const post = data.ghostPost
+  const parser = new DOMParser()
+  const firstParse = parser.parseFromString(post.html, "text/html")
+  const body = firstParse.getElementsByTagName("body")
+  const filmsCardHTML = firstParse.getElementsByTagName("figure")
+  const filmsHTML = firstParse.getElementsByTagName("iframe")
 
-    this.rootRef = React.createRef()
-
-    this.singleRefs = this.state.things.reduce((acc, value) => {
-      acc[value.id] = {
-        ref: createRef(),
-        id: value.id,
-        ratio: 0,
-      }
-
-      return acc
-    }, {})
-
-    const callback = entries => {
-      entries.forEach(
-        entry =>
-          (this.singleRefs[entry.target.id].ratio = entry.intersectionRatio)
-      )
-
-      const activeThing = Object.values(this.singleRefs).reduce(
-        (acc, value) => (value.ratio > acc.ratio ? value : acc),
-        this.state.activeThing
-      )
-
-      if (activeThing.ratio > this.state.activeThing.ratio) {
-        this.setState({ activeThing })
-      }
-    }
-    if (typeof window !== `undefined`) {
-      this.observer = new IntersectionObserver(callback, {
-        root: this.rootRef.current,
-        threshold: new Array(101).fill(0).map((v, i) => i * 0.01),
-      })
-    }
+  let films = []
+  for (let i = 0; i < filmsHTML.length; i++) {
+    filmsHTML[i].classList.add("single-film-container")
+    filmsCardHTML[i].setAttribute("id", `film-${i}`)
+    filmsHTML[i].removeAttribute("width")
+    filmsHTML[i].removeAttribute("height")
+    filmsCardHTML[i].classList.add("iframe-container")
+    films = [...films, filmsHTML[i]]
   }
+  useEffect(() => {
+    let rawElements = body[0].children
+    let elements = []
 
-  componentDidMount() {
-    if (typeof window !== `undefined`) {
-      Object.values(this.singleRefs).forEach(value =>
-        this.observer.observe(value.ref.current)
-      )
-      Object.keys(this.state.loading).forEach(id => {
-        document.getElementById(id).onload = () => {
-          this.setState({ loading: { [id]: false } })
-        }
-      })
+    for (let i = 0; i < rawElements.length; i++) {
+      elements.push(rawElements[i])
     }
-  }
+    elements.forEach(elem => {
+      console.log(elem)
+      rootRef.current.appendChild(elem)
+    })
+  }, [body])
 
-  render() {
-    return (
-      <Layout color="#1f2839" sub={"films"}>
-        <div className="film-numbers">
-          {this.state.things.map(({ id }) => (
-            <div key={id}>
-              <a
-                href={`#${id === "0" ? "title" : id}`}
-                style={{
-                  color: id === this.state.activeThing.id ? "grey" : "inherit",
-                }}
-              >
-                {parseInt(id) + 1}
-              </a>
-            </div>
-          ))}
-        </div>
-        <div className="scrollable-container film-container" ref={this.rootRef}>
-          <h1 id="title">{this.props.data.ghostPost.title}</h1>
-          {this.state.things.map(({ film, text, id }) => (
-            <div
-              ref={this.singleRefs[id].ref}
-              id={id}
-              key={id}
-              className="single-film-container"
-            >
-              <Loader loading={this.state.loading[`film-${id}`]} color="grey" />
-              <div
-                className="iframe-container"
-                style={{
-                  display: this.state.loading[`film-${id}`] ? "none" : "inline",
-                }}
-                dangerouslySetInnerHTML={{ __html: film }}
-              />
-              <div
-                className="text-container"
-                dangerouslySetInnerHTML={{ __html: text }}
-              />
-            </div>
-          ))}
-        </div>
-      </Layout>
-    )
-  }
+  return (
+    <Layout color="#1f2839" sub={"films"}>
+      {/* <div className="film-numbers">
+        {films.map((_, index) => (
+          <a href={`#film-${index}`}>{index + 1}</a>
+        ))}
+      </div> */}
+      <div className="scrollable-container film-container" ref={rootRef} />
+    </Layout>
+  )
 }
 
 export default FilmTemplate
