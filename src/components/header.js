9537c935898d6a66979d015c805af176acc6c6bb
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-10-30 00:06:17 +0200

    Fix font

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,12 @@ import { Helmet } from "react-helmet"
 const Head = () => {
   return (
     <Helmet defer={false} defaultTitle="Swantjes Website">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
-        href="https://fonts.googleapis.com/css?family=Josefin+Sans:100&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap"
         rel="stylesheet"
-      ></link>
+      />
     </Helmet>
   )
 }
